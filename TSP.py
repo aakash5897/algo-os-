695d4e044919c63905a585f1838ef3625554e8e3
aakash5897 <aakash.dokka@gmail.com>
2017-09-13 23:52:17 +0530

    added tsp

--- TSP.py
+++ TSP.py
@@ -2,13 +2,11 @@ class TSP:
 	def __init__(self,dat=None):
 		self.data={}
 		self.v=[]
-		self._e=[]
+		self.p=[]
+		self._c=0
 		try:
 			self.get_data(dat)
 		except:raise TypeError("Did not provide proper detailes")
-	@property
-	def infi(self):
-		return 999
 	def get_data(self,val=None):
 		print("provide the data regarding the graph like the following:\n[(vertex_1,vertex_2,weight of the edge)] example:\n('a','b',4) which represents an edge connecting two vertices a,b(as strings) with weight 4")	
 		l=eval(input())
@@ -16,10 +14,10 @@ class TSP:
 			for j in range(2):
 				if not i[j] in self.v:self.v.append(i[j])
 		self.res={(i[0],i[1]):i[2] for i in l}
-		print(self.res)
+		self.func()
 	def func(self):
 		self.am=[[] for i in range(len(self.v))]
-		temp=list(self.v)
+		temp=self.v
 		for i in self.am:
 			for j in range(len(temp)):
 				if (temp[self.am.index(i)],temp[j]) in self.res:
@@ -27,8 +25,56 @@ class TSP:
 				elif (temp[j],temp[self.am.index(i)]) in self.res:
 					i.append(self.res[(temp[j],temp[self.am.index(i)])])					
 				else:i.append(0)
-		return self.am
+		print(self.am)
+	def mak_sub(self,sub,l):
+		R=list(sub)
+		for i in range(len(R)):
+			temp=R[i:i+l]
+			if temp[0]==R[-1] and l!=1:temp=[R[i],R[0]]
+			self.p.append(set(temp))
+		r=lambda x:[i for i in x if len(i)==l]
+		return r(self.p)
+	def dist(self,li):
+		try:return self.res[(li[0],li[1])]
+		except KeyError:return self.res[(li[1],li[0])]
+	def g(self,el,S):
+		#_s=S
+		#_s.update("1")
+		if S==set():return self.dist([el,"1"])
+		else:
+			for j in S:
+				temp=S-{j}
+				if len(temp)==0:return self.dist([el,j])+self.dist([j,"1"])
+				else:return self.dist([el,j])+self.g(j,temp)
 	def algo(self):
+		assert self._c==0,("This function can be called only once")
+		self._c+=1
+		dist_dict={}
+		check_list=[]
+		rs=[]
+		S=set(self.v)-{"1"}
+		for _ in range(len(S)):
+			subs=self.mak_sub(S,_+1)
+			check_list.append(subs)
+		check_list=[i for _ in check_list for i in _]
+		for _ in check_list:
+			rt=[]
+			for i in _:
+				rt.append(self.g(i,_-{i}))
+			rs.append([rt])	
+		return rs						
+	
+				
+				
+				
+				
+				
+		
+			
+			
+		
+		
+			
 			
 			
 				
